39f60dd1d5d3184ff098a2a0b85df04acae30554
diego londoño <diegoferlondo13@gmail.com>
2022-11-27 17:26:19 -0500

    agrague una linea de codigo aclaratoria que es otra manera de mostar lo que ingresa por teclado desde la vista

--- vue.js
+++ vue.js
@@ -3,3 +3,5 @@ new Vue({
     data: {
         nombre: "Carlos"
     }})
+
+    // <input type="text" v-on:keyup=""> esta linea de codigo iria en el html y es l amanera sencilla de mostar lo que se escucha por teclado
